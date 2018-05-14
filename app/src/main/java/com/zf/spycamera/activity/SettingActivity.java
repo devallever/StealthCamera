2e423f7ee14563b5acac6288b77398fe9d1c0d09
dengyongquan <dengyq@izhifei.com>
2018-05-14 18:17:58 +0800

    BUGFIX:当点击隐藏按钮时，回到首页没有变成黄圆圈图标，当在隐藏模式，关闭隐藏模式切回去也没有出现小的相机

--- app/src/main/java/com/zf/spycamera/activity/SettingActivity.java
+++ app/src/main/java/com/zf/spycamera/activity/SettingActivity.java
@@ -1,6 +1,8 @@
 package com.zf.spycamera.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,14 +10,19 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import com.zf.spycamera.FloatWindowService;
 import com.zf.spycamera.R;
+import com.zf.spycamera.utils.FloatWindowUtil;
 import com.zf.spycamera.utils.SPUtil;
+import com.zf.spycamera.view.PreviewView;
+
+import java.util.Set;
 
 /**
  * Created by Allever on 18/5/11.
  */
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "SettingActivity";
     private RelativeLayout mRlPreviewCont;
     private ImageView mIvPreview;
@@ -36,25 +43,10 @@ public class SettingActivity extends AppCompatActivity {
     private void initView(){
         mRlPreviewCont = findViewById(R.id.id_setting_rl_preview_container);
         mIvPreview = findViewById(R.id.id_setting_iv_prieview);
+        mRlPreviewCont.setOnClickListener(this);
+        mIvPreview.setOnClickListener(this);
 
         refreshCb();
-
-        mIvPreview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SPUtil.setShowPreview( SettingActivity.this,!SPUtil.getShowPreview(SettingActivity.this));
-                refreshCb();
-            }
-        });
-
-        mRlPreviewCont.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SPUtil.setShowPreview( SettingActivity.this,!SPUtil.getShowPreview(SettingActivity.this));
-                refreshCb();
-            }
-        });
-
     }
 
     private void refreshCb(){
@@ -63,6 +55,26 @@ public class SettingActivity extends AppCompatActivity {
             mIvPreview.setImageResource(R.drawable.checkbox_on);
         }else {
             mIvPreview.setImageResource(R.drawable.checkbox_off);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.id_main_ll_menu_container:
+            case R.id.id_setting_iv_prieview:
+                SPUtil.setShowPreview( SettingActivity.this,!SPUtil.getShowPreview(SettingActivity.this));
+                refreshCb();
+                if (FloatWindowService.mService != null){
+                    //相机已启动
+                    Intent intent = new Intent(this, FloatWindowService.class);
+                    stopService(intent);
+                    startService(intent);
+                }
+                break;
+            default:
+                break;
         }
     }
 }
