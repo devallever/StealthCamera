dfd3a4effb339ed86118087ac6f1e2d291820d2d
dengyongquan <dengyq@izhifei.com>
2018-05-14 10:11:28 +0800

    OPTIMIZE: 注释计时器相关代码.

--- app/src/main/java/com/zf/spycamera/FloatWindowService.java
+++ app/src/main/java/com/zf/spycamera/FloatWindowService.java
@@ -7,20 +7,18 @@ import android.hardware.Camera;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.zf.spycamera.utils.FloatWindowUtil;
 
-import java.io.Flushable;
 import java.util.Timer;
-import java.util.TimerTask;
-
-import javax.xml.transform.sax.TemplatesHandler;
 
 /**
  * Created by Allever on 18/5/11.
  */
 
 public class FloatWindowService extends Service {
+    private static final String TAG = "FloatWindowService";
     private Timer mTimer;
     private Context mContext;
     private Handler mHandler = new Handler();
@@ -37,8 +35,8 @@ public class FloatWindowService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mTimer.cancel();
-        mTimer = null;
+//        mTimer.cancel();
+//        mTimer = null;
         CameraManager.getIns().releaseCamera();
 
         //关闭浮窗
@@ -48,12 +46,21 @@ public class FloatWindowService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (mTimer == null){
-            mTimer = new Timer();
-            mTimer.scheduleAtFixedRate(new RefreshTask(),0,100);
-        }
+//        if (mTimer == null){
+//            mTimer = new Timer();
+//            mTimer.scheduleAtFixedRate(new RefreshTask(),0,100);
+//        }
 
         CameraManager.getIns().openCamera(Camera.CameraInfo.CAMERA_FACING_BACK);
+        //没有悬浮窗显示，则创建悬浮窗。
+        if (!FloatWindowUtil.isFloatWindowShowing()) {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    FloatWindowUtil.createFloatWindow(getApplicationContext());
+                }
+            });
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -63,19 +70,20 @@ public class FloatWindowService extends Service {
         return null;
     }
 
-    class RefreshTask extends TimerTask{
-        @Override
-        public void run() {
-            // 当前界面是桌面，且没有悬浮窗显示，则创建悬浮窗。
-            if (!FloatWindowUtil.isFloatWindowShowing()) {
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        FloatWindowUtil.createFloatWindow(getApplicationContext());
-                    }
-                });
-            }
-        }
-    }
+//    class RefreshTask extends TimerTask{
+//        @Override
+//        public void run() {
+//            Log.d(TAG, "run: ");
+//            // 当前界面是桌面，且没有悬浮窗显示，则创建悬浮窗。
+//            if (!FloatWindowUtil.isFloatWindowShowing()) {
+//                mHandler.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        FloatWindowUtil.createFloatWindow(getApplicationContext());
+//                    }
+//                });
+//            }
+//        }
+//    }
 
 }
