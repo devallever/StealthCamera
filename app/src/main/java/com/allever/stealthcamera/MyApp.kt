cc46e5d5464b2dfca8290bc0f8f9e6b12a133d78
devallever <devallever@163.com>
2019-09-14 05:00:50 +0800

    内容：配置友盟和多渠道

--- app/src/main/java/com/allever/stealthcamera/MyApp.kt
+++ app/src/main/java/com/allever/stealthcamera/MyApp.kt
@@ -1,10 +1,12 @@
 package com.allever.stealthcamera
 
 import com.allever.lib.common.app.App
+import com.allever.lib.umeng.UMeng
 
 class MyApp: App() {
     override fun onCreate() {
         super.onCreate()
         com.android.absbase.App.setContext(this)
+        UMeng.init(this)
     }
 }
